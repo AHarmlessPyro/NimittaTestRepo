4f632093d4864d88cd56894097704032ba0ef100
sinha104 <sinha104@umn.edu>
2020-06-02 19:46:01 -0500

    Updates

---
+++ precache-manifest.f7d813ec30d2897015b91be20d8e8d54.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150626462fdde22decd07448b2920a94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7f7544706bd74ec3651",
+    "url": "/static/css/main.6a03316f.chunk.css"
+  },
+  {
+    "revision": "938bfba2a18758635a70",
+    "url": "/static/js/2.2b5f391b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b5f391b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f7544706bd74ec3651",
+    "url": "/static/js/main.7be74116.chunk.js"
+  },
+  {
+    "revision": "e43dca43503e7cd9a078",
+    "url": "/static/js/runtime-main.f58e752b.js"
+  },
+  {
+    "revision": "85c825e3eb2d224347cd485b9e12ffc6",
+    "url": "/static/media/1.85c825e3.png"
+  },
+  {
+    "revision": "0db124c3a972c879b3ed74310bf47176",
+    "url": "/static/media/2.0db124c3.png"
+  },
+  {
+    "revision": "88ff9bedc6af273e82dcd27ad108dd9e",
+    "url": "/static/media/3.88ff9bed.png"
+  }
+]);
