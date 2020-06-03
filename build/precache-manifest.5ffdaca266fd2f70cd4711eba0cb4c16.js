1f152598192c0f3a7cd2b2d0a549443b8f37a1c1
sinha104 <sinha104@umn.edu>
2020-06-02 20:51:50 -0500

    new deploy build

---
+++ build/precache-manifest.5ffdaca266fd2f70cd4711eba0cb4c16.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6370ec231e9209fed18c3bfdb0d7d796",
+    "url": "/NimittaTestRepo/index.html"
+  },
+  {
+    "revision": "ec11074d957a8f7c807a",
+    "url": "/NimittaTestRepo/static/css/main.a563fbf1.chunk.css"
+  },
+  {
+    "revision": "938bfba2a18758635a70",
+    "url": "/NimittaTestRepo/static/js/2.2b5f391b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NimittaTestRepo/static/js/2.2b5f391b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec11074d957a8f7c807a",
+    "url": "/NimittaTestRepo/static/js/main.bdc27487.chunk.js"
+  },
+  {
+    "revision": "10e2832b038f9a63b8d5",
+    "url": "/NimittaTestRepo/static/js/runtime-main.387aac8b.js"
+  },
+  {
+    "revision": "85c825e3eb2d224347cd485b9e12ffc6",
+    "url": "/NimittaTestRepo/static/media/1.85c825e3.png"
+  },
+  {
+    "revision": "0db124c3a972c879b3ed74310bf47176",
+    "url": "/NimittaTestRepo/static/media/2.0db124c3.png"
+  },
+  {
+    "revision": "88ff9bedc6af273e82dcd27ad108dd9e",
+    "url": "/NimittaTestRepo/static/media/3.88ff9bed.png"
+  }
+]);
